2f5bee28f7e57408e111b9b3d867c68879f22154
shuting <zst9394@outlook.com>
2021-05-09 21:43:16 -0400

    change style

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,14 +8,16 @@ class Portfolio extends React.Component {
   render() {
     return (
       <div className="card" id="portfolio">
+        <h1 className="card-title text-primary">My Recent Project:</h1>
         {this.state.projects.map((app, key) => (
-          <div className="card">
+          <div id="project">
             <div className="card-body" key={key}>
-              <h5 className="card-title">{app.app}</h5>
+              <h4 className="card-title text-primary">{app.app}</h4>
+              <a href={app.live} className="btn btn-success">Check the app</a>
+              <a href={app.git} className="btn btn-dark">Check the repo</a>
               <p className="card-text">{app.description}</p>
-              <img src={process.env.PUBLIC_URL+app.screenshot} className="card-img-top img-fluid" alt={app.app} />
-              <a href={app.live} className="btn btn-primary">Check the app</a>
-              <a href={app.git} className="btn btn-primary">Check the repo</a>
+              <img id="projectIMG" src={process.env.PUBLIC_URL+app.screenshot} className="card-img-top img-fluid" alt={app.app} />
+
             </div>
           </div>
         ))}
